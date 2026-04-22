84217b896aec1355bc2d45e46913813a9777787c
taminhduc2005 <taminhduc22072005@gmail.com>
2026-04-22 22:14:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import customtkinter as ctk
 from src.views.login_view import LoginView
 
 if __name__ == "__main__":
-    # Khởi tạo ứng dụng
     app = LoginView()
-    
-    # Chạy vòng lặp chính
     app.mainloop()
